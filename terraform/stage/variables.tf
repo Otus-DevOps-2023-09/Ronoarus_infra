9d9cc029a01178275d31222d36f47f041a72302f
DenisPoleshchuk <denis.poleschyk@gmail.com>
2023-11-06 14:48:15 +0400

    terraform-2 dz

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -35,3 +35,7 @@ variable "db_disk_image" {
   description = "Disk image for reddit db"
   default     = "reddit-db-base"
 }
+variable "environment" {
+  description = "Environment for name"
+  default     = "stage"
+}
